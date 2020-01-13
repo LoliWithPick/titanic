93c8e93dc6ef77076ee1991e2ce14ed55360b3af
Deny <820034113@qq.com>
2020-01-13 17:58:32 +0800

    finnished

--- main.py
+++ main.py
@@ -76,5 +76,126 @@ combine = [train_df, test_df]
 # print(train_df.shape, test_df.shape)
 for dataset in combine:
     dataset['Sex'] = dataset['Sex'].map({'female': 1, 'male': 0}).astype(int)
-print(train_df.head())
+# print(train_df.head())
+# grid = sns.FacetGrid(train_df, row='Pclass', col='Sex', size=2.2, aspect=1.6)
+# grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+# grid.add_legend()
+# plt.show()
+guess_ages = np.zeros((2,3))
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & 
+                                (dataset['Pclass'] == j + 1)]['Age'].dropna()
+            age_guess = guess_df.median()
+            guess_ages[i, j] = int(age_guess/0.5 + 0.5) * 0.5
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[(dataset.Age.isnull()) 
+                            & (dataset.Sex == i) 
+                            & (dataset.Pclass == j + 1)
+                            , 'Age'] = guess_ages[i, j]
+    dataset['Age'] = dataset['Age'].astype(int)
+for dataset in combine:
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+# print(train_df[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+for dataset in combine:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+# print(train_df[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+combine = [train_df, test_df]
+for dataset in combine:
+    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+freq_port = train_df.Embarked.dropna().mode()[0]
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+for dataset in combine:
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+x_train = train_df.drop("Survived", axis=1)
+y_train = train_df['Survived']
+x_test = test_df.drop("PassengerId", axis=1).copy()
+# print(x_train.shape, y_train.shape, x_test.shape)
+# Logistic Regression
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+y_pred = logreg.predict(x_test)
+acc_log = round(logreg.score(x_train, y_train) * 100, 2)
+# print(acc_log)
+coeff_df = pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns = ['Feature']
+coeff_df['Correlation'] = pd.Series(logreg.coef_[0])
+print(coeff_df.sort_values(by='Correlation', ascending=False))
 
+svc = SVC()
+svc.fit(x_train, y_train)
+acc_svc = round(svc.score(x_train, y_train) * 100, 2)
+print(acc_svc)
+
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+acc_knn = round(knn.score(x_train, y_train) * 100, 2)
+print(acc_knn)
+
+gaussian = GaussianNB()
+gaussian.fit(x_train, y_train)
+y_pred = gaussian.predict(x_test)
+acc_gaussian = round(gaussian.score(x_train, y_train) * 100, 2)
+print(acc_gaussian)
+
+perceptron = Perceptron()
+perceptron.fit(x_train, y_train)
+y_pred = perceptron.predict(x_test)
+acc_perceptron = round(perceptron.score(x_train, y_train) * 100, 2)
+print(acc_perceptron)
+
+linear_svc = LinearSVC()
+linear_svc.fit(x_train, y_train)
+y_pred = linear_svc.predict(x_test)
+acc_linear_svc = round(linear_svc.score(x_train, y_train) * 100, 2)
+print(acc_linear_svc)
+
+sgd = SGDClassifier()
+sgd.fit(x_train, y_train)
+y_pred = sgd.predict(x_test)
+acc_sgd = round(sgd.score(x_train, y_train) * 100, 2)
+print(acc_sgd)
+
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(x_train, y_train)
+y_pred = decision_tree.predict(x_test)
+acc_decision_tree = round(decision_tree.score(x_train, y_train) * 100, 2)
+print(acc_decision_tree)
+
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(x_train, y_train)
+y_pred = random_forest.predict(x_test)
+acc_random_forest = round(random_forest.score(x_train, y_train) * 100, 2)
+print(acc_random_forest)
+models = pd.DataFrame({
+    'Model': ['SVC', 'KNN', 'LRG', 'RF', 'NB', 'PERC', 'SGD', 'Linear SVC', 'DT'],
+    'Score': [acc_svc, acc_knn, acc_log, acc_random_forest,
+                acc_gaussian, acc_perceptron, acc_sgd, acc_linear_svc,
+                acc_decision_tree]
+})
+print(models.sort_values(by='Score', ascending=False))
+submission = pd.DataFrame({
+    'PassengerId': test_df['PassengerId'],
+    'Survived': y_pred
+})
+print(submission)
